17185b4ba352f1dbd002eccf790b81c4b465742c
Diksha <dikshaofficial456@gmail.com>
2025-06-29 13:56:44 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './app.js';
 
 // This is the root element from our public/index.html file
 const root = ReactDOM.createRoot(document.getElementById('quiznova-admin-app'));
